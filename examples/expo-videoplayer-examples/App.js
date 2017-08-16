0c95bb9b7e200bbca8c835aa76a27f6b3484c6af
Abi Raja <abimanyuraja@gmail.com>
2017-08-16 11:48:08 -0700

    Create a great example

--- examples/expo-videoplayer-examples/App.js
+++ examples/expo-videoplayer-examples/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, View } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
@@ -18,10 +18,11 @@ export default class App extends React.Component {
       return <AppLoading />;
     } else {
       return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' &&
-            <View style={styles.statusBarUnderlay} />}
+        <View style={{
+          flex: 1,
+          backgroundColor: '#fff'
+        }}>
+          <StatusBar hidden={true} />
           <RootNavigation />
         </View>
       );
@@ -49,14 +50,3 @@ export default class App extends React.Component {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  statusBarUnderlay: {
-    height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
-});
--- examples/expo-videoplayer-examples/navigation/RootNavigation.js
+++ examples/expo-videoplayer-examples/navigation/RootNavigation.js
@@ -1,11 +1,9 @@
-import { Notifications } from 'expo';
 import React from 'react';
-import { Platform, ScrollView, View } from 'react-native';
+import { Platform, Dimensions, ScrollView, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
-import { Video } from 'expo';
+import { Video, ScreenOrientation } from 'expo';
 import VideoPlayer from 'abi-expo-videoplayer';
-
 
 const tintColor = '#2f95dc';
 
@@ -27,32 +25,79 @@ var styles = {
     flex: 1,
     backgroundColor: '#fff',
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
 };
 
-class DefaultScreen extends React.Component {
-  static navigationOptions = {
+class BaseScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
     header: null,
+    tabBarVisible: navigation.state.params && navigation.state.params.tabBarHidden ? false : true,
+  });
+
+  state = {
+    isPortrait: true,
   };
 
+  componentDidMount() {
+    ScreenOrientation.allow(ScreenOrientation.Orientation.ALL);
+    Dimensions.addEventListener(
+      'change',
+      this.orientationChangeHandler.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT);
+    Dimensions.removeEventListener('change', this.orientationChangeHandler);
+  }
+
+  orientationChangeHandler(dims) {
+    const { width, height } = dims.window;
+    const isLandscape = width > height;
+    this.setState({ isPortrait: !isLandscape });
+    this.props.navigation.setParams({ tabBarHidden: isLandscape});
+    ScreenOrientation.allow(ScreenOrientation.Orientation.ALL);
+  }
+
+  switchToLandscape() {
+    ScreenOrientation.allow(ScreenOrientation.Orientation.LANDSCAPE);
+  }
+
+  switchToPortrait() {
+    ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT);
+  }
+}
+
+class CustomScreen extends BaseScreen {
   render() {
+    const COLOR = '#92DCE5';
+    const icon = (name, size=36) =>
+      () => <Ionicons name={name} size={size} color={COLOR} style={{ textAlign: 'center' }} />
     return (
       <View style={styles.container}>
         <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}>
+          style={styles.container}>
           <VideoPlayer
             videoProps={{
-              shouldPlay: true,
+              shouldPlay: false,
               resizeMode: Video.RESIZE_MODE_CONTAIN,
               source: {
-                uri: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+                uri:  'http://www.streambox.fr/playlists/test_001/stream.m3u8',
               },
-              isMuted: true
+              isMuted: false
             }}
-            isPortrait={true}
+            playIcon={icon('ios-play-outline')}
+            pauseIcon={icon('ios-pause-outline')}
+            fullscreenEnterIcon={icon('ios-expand-outline', 28)}
+            fullscreenExitIcon={icon('ios-contract-outline', 28)}
+            trackImage={require('../assets/track.png')}
+            thumbImage={require('../assets/thumb.png')}
+            textStyle={{
+              color: COLOR,
+              fontSize: 12,
+            }}
+            isPortrait={this.state.isPortrait}
+            switchToLandscape={this.switchToLandscape.bind(this)}
+            switchToPortrait={this.switchToPortrait.bind(this)}
             playFromPositionMillis={0}
           />
 
@@ -62,11 +107,7 @@ class DefaultScreen extends React.Component {
   }
 }
 
-class CustomScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
+class DefaultScreen extends BaseScreen {
   render() {
     return (
       <View style={styles.container}>
@@ -78,11 +119,13 @@ class CustomScreen extends React.Component {
               shouldPlay: true,
               resizeMode: Video.RESIZE_MODE_CONTAIN,
               source: {
-                uri: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+                uri: 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
               },
-              isMuted: true
+              isMuted: false
             }}
-            isPortrait={true}
+            isPortrait={this.state.isPortrait}
+            switchToLandscape={this.switchToLandscape.bind(this)}
+            switchToPortrait={this.switchToPortrait.bind(this)}
             playFromPositionMillis={0}
           />
 
@@ -152,7 +195,6 @@ const RootStackNavigator = StackNavigator(
 );
 
 export default class RootNavigator extends React.Component {
-
   render() {
     return <RootStackNavigator />;
   }
