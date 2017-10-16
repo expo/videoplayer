55eea4fd43fd2213320c49858934de96c442d721
nattatorn.dev <nattatorn.dev@gmail.com>
2017-10-17 02:17:48 +0700

    ✨ Allow hide fullscreen button in video control

--- index.js
+++ index.js
@@ -91,6 +91,11 @@ export default class VideoPlayer extends React.Component {
     */
     errorCallback: PropTypes.func,
 
+    /**
+    * Allow fullscreen button
+    */
+    fs: PropTypes.bool,
+
     // Icons
     playIcon: PropTypes.func,
     pauseIcon: PropTypes.func,
@@ -158,6 +163,8 @@ export default class VideoPlayer extends React.Component {
       );
     },
     showControlsOnLoad: false,
+    // Control Options
+    fs: true,
   };
 
   constructor(props) {
@@ -717,7 +724,7 @@ export default class VideoPlayer extends React.Component {
             </Text>
 
             {/* Fullscreen control */}
-            <Control
+            {this.props.fs && <Control
               style={{ backgroundColor: 'transparent' }}
               center={false}
               callback={() => {
@@ -728,7 +735,7 @@ export default class VideoPlayer extends React.Component {
               {this.props.isPortrait
                 ? <FullscreenEnterIcon />
                 : <FullscreenExitIcon />}
-            </Control>
+            </Control>}
           </Animated.View>
         </View>
       </TouchableWithoutFeedback>
